db3d0cf47bcee0c20ad953628d95aa923301fb6d
Benjamin Howard <bentlife@icloud.comm>
2022-04-12 15:47:03 -0700

    Determine which predictor to use for each frame -- not tested.

--- wav_compression.cpp
+++ wav_compression.cpp
@@ -3,12 +3,35 @@
 #include <array>
 #include <vector>
 #include <stdexcept>
+#include <utility>
+#include <cmath>
 
 using std::invalid_argument;
 using std::array;
 using std::vector;
+using std::pair;
 
-struct CompressedWAV{};
+template<class T>
+struct Frame {
+    bool is_constant;
+    quint8 p;
+    vector<T> first_p_samples;
+    vector<T> residuals;
+
+    // TODO: Determine num bits to store m.
+    int m;
+};
+
+template<class T>
+struct CompressedWAV {
+    quint32 n;
+    quint16 num_samples_per_frame;
+    quint16 bit_depth;
+    vector<Frame<T>> frames;
+
+    // These bytes shall be the final output for the compressed audio file.
+    vector<quint8> bytes;
+};
 
 const quint16 NUM_SAMPLES_PER_FRAME = 1152;
 const quint8 NUM_PREDICTORS = 4;
@@ -37,20 +60,16 @@ T predict(quint8 p, quint16 n, const vector<T>& x_hat) {
 
 template<class T>
 void init_predictions(quint8 p, quint32 n, vector<T>& x_hat, const T* samples) {
-//    quint32 i;
     // TODO: What if n is not divisible by num_samples_per_frame? Then this access may go out of bounds?
     for (quint32 j = 0; j < n; j += NUM_SAMPLES_PER_FRAME) {
         for (quint32 i = 0; i < p; i++) {
             x_hat[j + i] = samples[j + i];
         }
     }
-//    for (; i < n; i++) {
-//        x_hat.push_back(0);
-//    }
 }
 
 template<class T>
-void compress(const T* samples, quint32 n, CompressedWAV& out_c_wav) {
+void compress(const T* samples, quint32 n, CompressedWAV<T>& out_c_wav) {
     array<vector<T>, NUM_PREDICTORS> p;
     for (quint8 i = 0; i < NUM_PREDICTORS; i++) {
         p[i] = vector<T>(n);
@@ -71,10 +90,44 @@ void compress(const T* samples, quint32 n, CompressedWAV& out_c_wav) {
         }
     }
 
+    // Determine which predictor to use for each frame.
+    for (quint32 i = 0; i < n; i += NUM_SAMPLES_PER_FRAME) {
+        quint32 start_of_next_frame = i + NUM_SAMPLES_PER_FRAME;
+        pair<quint8, quint64> min_err = {0, LONG_MAX};
+        for (quint8 j = 0; j < NUM_PREDICTORS; j++) {
+            quint64 err_sum = 0;
+            for (quint32 k = i; k < start_of_next_frame; k++) {
+                err_sum += abs(e[i][k]);
+            }
+            if (err_sum < min_err.second) {
+                min_err.first = i;
+                min_err.second = err_sum;
+            }
+        }
+        Frame<T> frame;
+        frame.is_constant = (min_err.first == 0);
+        frame.p = min_err.first;
+        vector<T> first_p_samples;
+        for (quint32 j = 0, k = i; j < frame.p; j++, k++) {
+            first_p_samples.push_back(samples[k]);
+        }
+        vector<T> residuals;
+        for (quint32 j = frame.p; j < start_of_next_frame; j++) {
+            residuals.push_back(e[frame.p][j]);
+        }
+        frame.first_p_samples = first_p_samples;
+        frame.residuals = residuals;
+        out_c_wav.frames.push_back(frame);
+    }
+}
+
+template<class T>
+void encode(CompressedWAV<T>& out_c_wav) {
 
 }
 
-void compress(const WAV& wav, CompressedWAV& out_c_wav) {
+template<class T>
+void compress(const WAV& wav, CompressedWAV<T>& out_c_wav) {
     switch (wav.bits_per_sample) {
         case 8:
             compress((quint8*) wav.bytes.get(), wav.data_size, out_c_wav);
@@ -88,6 +141,7 @@ void compress(const WAV& wav, CompressedWAV& out_c_wav) {
     }
 }
 
-void decompress(const CompressedWAV& c_wav, WAV& out_wav) {
+template<class T>
+void decompress(const CompressedWAV<T>& c_wav, WAV& out_wav) {
 
 }
