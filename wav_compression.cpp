82d0ed4792b64e602680df9fc9f1d120ca684ddc
Benjamin Howard <bentlife@icloud.comm>
2022-04-12 21:09:12 -0700

    Write encode function and calculate m during compress() -- untested.

--- wav_compression.cpp
+++ wav_compression.cpp
@@ -3,13 +3,11 @@
 #include <array>
 #include <vector>
 #include <stdexcept>
-#include <utility>
 #include <cmath>
 
 using std::invalid_argument;
 using std::array;
 using std::vector;
-using std::pair;
 
 template<class T>
 struct Frame {
@@ -17,9 +15,7 @@ struct Frame {
     quint8 p;
     vector<T> first_p_samples;
     vector<T> residuals;
-
-    // TODO: Determine num bits to store m.
-    int m;
+    quint8 m;
 };
 
 template<class T>
@@ -93,20 +89,22 @@ void compress(const T* samples, quint32 n, CompressedWAV<T>& out_c_wav) {
     // Determine which predictor to use for each frame.
     for (quint32 i = 0; i < n; i += NUM_SAMPLES_PER_FRAME) {
         quint32 start_of_next_frame = i + NUM_SAMPLES_PER_FRAME;
-        pair<quint8, quint64> min_err = {0, LONG_MAX};
+        array<quint64, 3> min_err = {0, LONG_MAX, 0};
         for (quint8 j = 0; j < NUM_PREDICTORS; j++) {
             quint64 err_sum = 0;
             for (quint32 k = i; k < start_of_next_frame; k++) {
                 err_sum += abs(e[i][k]);
             }
-            if (err_sum < min_err.second) {
-                min_err.first = i;
-                min_err.second = err_sum;
+            if (err_sum < min_err[1]) {
+                min_err[0] = i; // p
+                min_err[1] = err_sum;
+                min_err[2] = (quint8) ceil(log2(log(2) * ((double) err_sum / NUM_SAMPLES_PER_FRAME))); // m
             }
         }
         Frame<T> frame;
-        frame.is_constant = (min_err.first == 0);
-        frame.p = min_err.first;
+        frame.is_constant = (min_err[0] == 0);
+        frame.p = min_err[0];
+        frame.m = min_err[2];
         vector<T> first_p_samples;
         for (quint32 j = 0, k = i; j < frame.p; j++, k++) {
             first_p_samples.push_back(samples[k]);
@@ -121,11 +119,73 @@ void compress(const T* samples, quint32 n, CompressedWAV<T>& out_c_wav) {
     }
 }
 
-template<class T>
-void encode(CompressedWAV<T>& out_c_wav) {
-
+// Function assumes n >= 8.
+quint8 next_byte(int& n, quint64 bits) {
+    quint64 mask = 0xFFu << (n - 8);
+    quint8 byte = (bits & mask) >> (n - 8);
+    n -= 8;
+    return byte;
 }
 
+void save_pending_bytes(vector<quint8>& bytes, quint64& b, int& n, quint64 bits) {
+    while (n >= 8) bytes[b++] = next_byte(n, bits);
+}
+
+void add_bits(int& n, quint64& bits, quint64 num_new_bits, quint64 x) {
+    bits <<= num_new_bits;
+    bits += x;
+    n += num_new_bits;
+}
+
+template<class T>
+void encode(CompressedWAV<T>& out_c_wav) {
+    const int NUM_M_BITS = 6;
+    vector<quint8> bytes;
+    // TODO: Write the file header to bytes.
+
+    // Encode each frame.
+    // Num bytes saved to vector.
+    quint64 b = 0; // TODO: won't be zero after writing to file header.
+    for (auto frame : out_c_wav.frames) {
+        auto p = frame.p;
+        auto m = frame.m;
+        int n = 2; // Num bits.
+        quint64 bits = p;
+
+        add_bits(n, bits, NUM_M_BITS, m);
+        save_pending_bytes(bytes, b, n, bits);
+
+        // TODO: Do something different if frame.is_constant.
+
+        // Store the first p samples into bytes.
+        for (int i = 0; i < p; i++) {
+            add_bits(n, bits, out_c_wav.bit_depth, frame.first_p_samples[i]);
+            save_pending_bytes(bytes, b, n, bits);
+        }
+
+        // Rice Encoding.
+        // Create a mask with m 1s in the least significant bits.
+        quint64 m_mask = 0;
+        for (int i = 0; i < m; i++) {
+            m_mask <<= 1;
+            m_mask++;
+        }
+        // Encode each residual.
+        for (auto res : frame.residuals) {
+            quint8 sign = (res >= 0) ? 0 : 1;
+            quint8 m_bits = res & m_mask;
+            quint64 num_zeros = abs((res & ~m_mask) >> m);
+
+            add_bits(n, bits, 1, sign);
+            add_bits(n, bits, m, m_bits);
+            add_bits(n, bits, num_zeros + 1, 1);
+
+            save_pending_bytes(bytes, b, n, bits);
+        }
+    }
+}
+
+// Kinda weird to do this since we have T already.
 template<class T>
 void compress(const WAV& wav, CompressedWAV<T>& out_c_wav) {
     switch (wav.bits_per_sample) {
